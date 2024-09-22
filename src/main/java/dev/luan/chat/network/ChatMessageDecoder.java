d6d3a4d4e76246df28ec473457eb41004a9c7971
luannndev <106454269+luannndev@users.noreply.github.com>
2024-09-22 19:50:28 +0200

    Add ChatMessageDecoder to handle incoming chat messages
    This new class decodes incoming byte buffers into ChatMessage objects using Netty's ByteToMessageDecoder. It extracts the sender and message from the buffer, constructs a ChatMessage, and adds it to the list of decoded messages.

---
+++ src/main/java/dev/luan/chat/network/ChatMessageDecoder.java
@@ -0,0 +1,19 @@
+package dev.luan.chat.network;
+
+import dev.luan.chat.message.ChatMessage;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.ByteToMessageDecoder;
+
+import java.util.List;
+
+public class ChatMessageDecoder extends ByteToMessageDecoder {
+    @Override
+    protected void decode(ChannelHandlerContext ctx, ByteBuf buffer, List<Object> list) throws Exception {
+        int senderLength = buffer.readInt();
+        String sender = buffer.readCharSequence(senderLength, ChatMessage.CHARSET).toString();
+        int messageLength = buffer.readInt();
+        String msg = buffer.readCharSequence(messageLength, ChatMessage.CHARSET).toString();
+        list.add(new ChatMessage(sender, msg));
+    }
+}
